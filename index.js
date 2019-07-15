f11ed566c92bf03670148cf29ea4ac9194aaabfe
Shahidh K Muhammed <shahidh@hasura.io>
2019-07-15 15:47:57 +0530

    remove hard-check for hasura org members

--- index.js
+++ index.js
@@ -14,56 +14,75 @@ octokit.authenticate({
   token: GITHUB_TOKEN
 });
 
+// a new pull request event
 webhooks.on('pull_request', async ({id, name, payload}) => {
-  if (name === 'pull_request') {
-    const {action, number, repository, sender} = payload;
-    const {pull_request: {merged, labels, user: {login}}} = payload;
 
-    let hfu = false;
-    try {
-      hfu = await isHacktoberfestUser(login);
-    } catch (error) {
-      console.error(error);
+  // extract relevant information
+  const {action, number, repository, sender} = payload;
+  const {pull_request: {merged, labels, user: {login}}} = payload;
+
+  // check if user is registered for Hacktoberfest
+  // disabled until next hacktoberfest
+  // let hfu = await isHacktoberfestUser(login);
+  let hfu = false;
+
+  // check if the user is a Hasura org member
+  let isHasuraOrgMember = false;
+  try {
+    let result = await octokit.orgs.checkMembership({
+      org: 'hasura',
+      username: login
+    });
+    if (result.status === 204 || result.status === 302) {
+      isHasuraOrgMember = true;
     }
+  } catch (e) {
+    isHasuraOrgMember = false;
+  }
 
-    if(isHasuraOrgMember(login)) {
-      return;
-    }
+  // do nothing if the user is a Hasura org member
+  if(isHasuraOrgMember) {
+    return;
+  }
 
-    // pull_requst opened (not from hasura)
-    if (action === 'opened') {
-      const body = `Beep boop! :robot:
+  // pull_requst opened event (not from hasura)
+  if (action === 'opened') {
+    const body = `Beep boop! :robot:
 
 Hey @${login}, thanks for your PR!
 
 One of my human friends will review this PR and get back to you as soon as possible. :clock1:
 
 Stay awesome! :sunglasses:`;
-      try {
-        const result = await octokit.issues.createComment({
-          owner: repository.owner.login, 
-          repo: repository.name, 
-          number, 
-          body
-        });
-      } catch (error) {
-        console.error(error);
-      }
+
+    // comment on the PR
+    const result = await octokit.issues.createComment({
+      owner: repository.owner.login,
+      repo: repository.name,
+      number,
+      body
+    });
+
+  }
+
+  // pull_request closed
+  if (action === 'closed') {
+
+    // closed by author themself, do nothing
+    if (sender.login === login) {
+      return;
     }
 
-    // pull_request closed
-    if (action === 'closed') {
-      if (sender.login === login) {
-        return;
-      }
-      if (isInvalid(labels)) {
-        return;
-      }
+    // pr has an invalid label, do nothing
+    if (isInvalid(labels)) {
+      return;
+    }
 
-      let body;
+    let body;
 
-      if (!merged && hfu) {
-        body = `Beep boop! :robot:
+    // pr is not merged and user is registered for hacktoberfest
+    if (!merged && hfu) {
+      body = `Beep boop! :robot:
 
 Hey @${login}!
 
@@ -71,50 +90,34 @@ Sorry that your PR wasn’t merged. :cry:
 
 Do take a look at any of the other open issues to see if you’d like to take something up! We’re around on [Discord](https://discord.gg/3FNQnWj) & [Twitter](https://twitter.com/HasuraHQ) if you have any questions :smile:`;
 
-      }
+    }
 
-      if (merged) {
-        body = `Beep boop! :robot:
+    // pr is merged
+    if (merged) {
+      body = `Beep boop! :robot:
 
 ![GIF](https://media1.tenor.com/images/15ae412a294bf128f6ba7e60aa0ea8e1/tenor.gif)
 
 Awesome work @${login}! All of us at Hasura :heart: what you did.
 
 Thanks again :hugs:`;
-      }
-
-      try {
-        const result = await octokit.issues.createComment({
-          owner: repository.owner.login, 
-          repo: repository.name, 
-          number, 
-          body
-        });
-      } catch (error) {
-        console.error(error);
-      }
     }
+
+    // comment with a thank you note
+    const result = await octokit.issues.createComment({
+      owner: repository.owner.login,
+      repo: repository.name,
+      number,
+      body
+    });
   }
 
-  console.log(name, 'event received');
+  console.log('event received');
 });
 
 webhooks.on('error', (error) => {
   console.log(`Error occured in "${error.event.name} handler: ${error.stack}"`);
 });
-
-const isHasuraOrgMember = (name) => {
-  const HASURA_ORG_MEMBERS = [
-    'coco98', 'rikinsk', 'ecthiender', 
-    'rajoshighosh', 'shahidhk', 'achocolatebear',
-    '0x777', 'tirumaraiselvan', 'rakeshkky', 
-    'praveenweb', 'arvi3411301', 'nizar-m', 
-    'anandfeb25', 'dsandip', 'karthikvt26',
-    'surendran82', 'wawhal', 'shark-h', 
-    'hasura-bot', 'AnnieAtHasura', 'vnovick'
-  ];
-  return Boolean(HASURA_ORG_MEMBERS.find(member => member === name))
-};
 
 const isInvalid = (labels) => {
   for (label of labels) {
