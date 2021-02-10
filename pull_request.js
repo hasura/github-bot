ec8fed82ca0973844854358ae8b8cf2addf29145
Vishnu Bharathi P <vishnubharathi04@gmail.com>
2021-02-10 17:15:38 +0530

    moves workflow dispatch logic to separate file for reuse

--- pull_request.js
+++ pull_request.js
@@ -1,24 +1,7 @@
 // pull_request.js: all PR related handlers
 
 import { prOpened, prClosed, prNovalue, prMerged } from './messages';
-
-function monoRepoWorkflowDispatch(octokit, name) {
-  return async (inputs) => {
-    try {
-      console.log(`dispatching github workflow: ${name}`);
-      await octokit.actions.createWorkflowDispatch({
-        owner: 'hasura',
-        repo: 'graphql-engine-mono',
-        workflow_id: `${name}.yml`,
-        ref: 'main',
-        inputs,
-      });
-    } catch (e) {
-      console.error(`failed to dispatch github workflow: ${name}`);
-      console.error(e);
-    }
-  };
-}
+import { monoRepoWorkflowDispatch } from './github_action';
 
 const pullRequestHandler = (octokit) => {
   const shadowOssPr = monoRepoWorkflowDispatch(octokit, 'migrate-hge-pr');
